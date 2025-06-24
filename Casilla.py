b3ad865a9c4b2ec622c02eb2f015d2687a6991b3
Aaron Garibay <ing.aarongaribay@outlook.com>
2025-06-24 13:05:51 -0700

    borrando debugs

--- Casilla.py
+++ Casilla.py
@@ -16,7 +16,5 @@ class Casilla:
             self.fichas.append(ficha)
 
     def quitar_ficha(self, ficha: 'Ficha'):
-        print(ficha)
-        print(self.fichas)
         if ficha in self.fichas:
             self.fichas.remove(ficha)
