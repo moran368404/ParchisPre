428fecbab0de94d7a66f71eb1370bdf34126ceba
moran368404 <moran368404@uabc.edu.mx>
2025-06-25 11:16:55 +0200

    Pequeños cambios finales

--- CasillaSalida.py
+++ CasillaSalida.py
@@ -2,6 +2,10 @@ from Casilla import Casilla
 
 
 class CasillaSalida(Casilla):
+    """""
+    Representa las casillas de donde los jugadores comienzan el juego, los jugadores llegan a ella cada que 
+    sacan una ficha obteniendo un 5 en el dado
+    """""
     def __init__(self, posicion: int):
         super().__init__(posicion)
         self.esta_activada = False
--- VistaJuego.py
+++ VistaJuego.py
@@ -9,14 +9,6 @@ from PyQt5.QtCore import QTimer, QPoint, QRect
 from PyQt5.QtGui import QPainter, QColor, QPen, QIcon, QPixmap
 from formulario_jugador import Ui_DialogJugador
 
-"""class VistaBase(QMainWindow):
-    def mostrar_mensaje(self, mensaje: str):
-        if hasattr(self.ui, 'mensajes'):
-            self.ui.mensajes.setText(mensaje)
-        else:
-            QMessageBox.information(self, "Mensaje", mensaje)"""
-
-
 class VistaBase(QMainWindow):
     def mostrar_mensaje(self, mensaje: str):
         mensajes_label = self.findChild(QLabel, "mensajes")
@@ -57,17 +49,6 @@ class VistaParchis(VistaBase):
         self.close()
         exit()
 
-    """def mostrar_tablero(self, tablero: Tablero):
-        self.mostrar_mensaje("\nTablero:")
-        for i, casilla in enumerate(tablero.casillas):
-            if casilla.fichas:
-                fichas_info = ', '.join(f"Ficha {f.id} ({f.jugador.nombre})" for f in casilla.fichas)
-                self.mostrar_mensaje(f"Casilla {i}: {fichas_info}")"""
-
-    """def actualizar_tablero(self):
-        pass"""
-
-
 # --- Clase para la interfaz Tablero (ui_tablero) ---
 
 class VistaTablero(VistaBase):
@@ -87,22 +68,6 @@ class VistaTablero(VistaBase):
         }
 
         self.btnLanzarDado.clicked.connect(self.lanzar_dado)
-
-    """def mostrar_ficha_en_casilla(self, numero_casilla, color_pion):
-        casilla_btn = getattr(self, f"casilla{numero_casilla}", None)
-        if casilla_btn:
-            icon_path = f"ficheros_ui/imagenes/pion_{color_pion}.png"
-            icon = QIcon(QPixmap(icon_path).scaled(30, 30))
-            casilla_btn.setIcon(icon)
-            casilla_btn.setIconSize(casilla_btn.size())
-        else:
-            print(f"Casilla {numero_casilla} no encontrada.")"""
-
-    """def limpiar_casilla(self, numero_casilla):
-        casilla_btn = getattr(self, f"casilla{numero_casilla}", None)
-        if casilla_btn:
-            casilla_btn.setIcon(QIcon())
-            casilla_btn.setStyleSheet("")"""
 
     def mostrar_turno(self, nombre_jugador, color_jugador):
         label_turno = self.findChild(QLabel, "label_turno")
@@ -270,74 +235,13 @@ class VistaTablero(VistaBase):
                         base_label.setPixmap(pixmap.scaled(30, 30))
                         base_label.setVisible(True)
 
-    """def position_to_coords(self, pos):
-        # Suponga que su tablero tiene una cuadrícula de 10x10, cada cuadrado es de 40x40 píxeles.
-        case_taille = 40
-        ligne = pos // 10
-        colonne = pos % 10
-        x = colonne * case_taille
-        y = ligne * case_taille
-        return x, y"""
-
     def conectar_senales(self, presenter):
         self.presenter = presenter
 
-    """def abrir_numero_jugadores(self):
-        self.ventana_jugadores = VentanaJugadores(self.presenter)
-        self.close()
-        self.ventana_jugadores.show()"""
 
     def cerrar(self):
         self.close()
         exit()
-
-    """def mostrar_tablero(self, tablero: Tablero):
-        self.mostrar_mensaje("\nTablero:")
-        for i, casilla in enumerate(tablero.casillas):
-            if casilla.fichas:
-                fichas_info = ', '.join(f"Ficha {f.id} ({f.jugador.nombre})" for f in casilla.fichas)
-                self.mostrar_mensaje(f"Casilla {i}: {fichas_info}")"""
-
-    """def mostrar_dado(self, resultado: int):
-        self.ui.dado.setText(f"{resultado}")"""
-
-    """def actualizar_tablero(self):
-        total_casillas = 68
-        # Limpiar primero todas las casillas
-        for i in range(1, total_casillas + 1):
-            self.limpiar_casilla(i)
-
-        # Visualizar las fichas
-        for jugador in self.juego.jugadores:
-            for ficha in jugador.fichas:
-                if ficha.posicion >= 0:
-                    index = ficha.posicion + 1  # porque casilla1 corresponde a la posición 0
-                    nombre_casilla = f"casilla{index}"
-                    boton = self.vista_tablero.findChild(QPushButton, nombre_casilla)
-
-                    if boton:
-                        color = ficha.color
-                        pixmap = QPixmap(f"ficheros_ui/imagenes/pion_{color}.png")
-                        icon = QIcon(pixmap)
-                        boton.setIcon(icon)
-                        boton.setIconSize(QSize(30, 30))
-                    else:
-                        print(f"Boton '{nombre_casilla}' irrastreable")"""
-
-    """def leer_input_jugador(self):
-        texto = self.lineedit_pieza = QtWidgets.QLineEdit(...)
-        if texto.isdigit():
-            ficha_id = int(texto)
-            self.presenter.mover_ficha_actual(ficha_id)
-            self.ui.inputJugador.clear()
-        else:
-            self.mostrar_mensaje("Por favor, introduce un número válido (0-3).")"""
-
-    """def lanzar_dado(self):
-        resultado = self.presenter.lanzar_dado()
-        self.mostrar_dado(resultado)
-        self.mostrar_mensaje(f"Tira el dado: {resultado}")"""
-
 
 # --- Clase para la ventana de selección del número de jugadores ---
 class VentanaJugadores(VistaBase):
